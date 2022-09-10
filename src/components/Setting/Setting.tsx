b99ae7f4efa77dd095735271d58754d6e19a48db
bryzZz <tebloev.nikita@mail.ru>
2022-09-10 17:49:26 +0800

    range slider design, Setting style refactoring

--- src/components/Setting/Setting.tsx
+++ src/components/Setting/Setting.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import ReactSelect from "react-select";
 import "./style.css";
 
@@ -21,31 +21,51 @@ export namespace Setting {
 
     export const Input: React.FC<InputProps> = memo(
         ({ label, value, min, max, onChange }) => {
+            const [isRangeHover, setIsRangeHover] = useState<boolean>(false);
+            const [isRangeActive, setIsRangeActive] = useState<boolean>(false);
+
             const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
                 onChange(+e.target.value);
             };
 
+            const handleMouseEnter = () => {
+                setIsRangeHover(true);
+            };
+            const handleMouseLeave = () => {
+                setIsRangeHover(false);
+            };
+            const handleMouseDown = () => {
+                setIsRangeActive(true);
+            };
+            const handleMouseUp = () => {
+                setIsRangeActive(false);
+            };
+
             return (
                 <div className='Setting'>
-                    <label className='Setting__label'>
-                        {label}
-                        <input
-                            className='Setting__input'
-                            type='number'
-                            min={min}
-                            max={max}
-                            value={value}
-                            onChange={handleChange}
-                        />
-                    </label>
+                    <span className='Setting__label'>{label}</span>
                     <input
-                        className='Setting__range'
+                        className='Setting__input'
+                        type='number'
+                        min={min}
+                        max={max}
+                        value={value}
+                        onChange={handleChange}
+                    />
+                    <input
+                        className={`Setting__range ${
+                            isRangeHover ? "hover" : ""
+                        } ${isRangeActive ? "active" : ""}`}
                         type='range'
                         min={min}
                         max={max}
                         step={1}
                         value={value}
                         onChange={handleChange}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                        onMouseDown={handleMouseDown}
+                        onMouseUp={handleMouseUp}
                     />
                 </div>
             );
@@ -63,17 +83,15 @@ export namespace Setting {
         };
 
         return (
-            <div className='Setting Setting--select'>
-                <label className='Setting__label'>
-                    {label}
-                    <ReactSelect
-                        defaultValue={defaultValue}
-                        onChange={handleChange}
-                        options={options}
-                        className='Setting-select-container'
-                        classNamePrefix='Setting-select'
-                    />
-                </label>
+            <div className='Setting'>
+                <span className='Setting__label'>{label}</span>
+                <ReactSelect
+                    defaultValue={defaultValue}
+                    onChange={handleChange}
+                    options={options}
+                    className='Setting-select-container'
+                    classNamePrefix='Setting-select'
+                />
             </div>
         );
     };
